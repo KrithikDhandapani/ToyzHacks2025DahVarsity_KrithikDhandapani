badcf842f6b1f6c1e342445f6db4d852f836e083
IamCurlyHair <95255679+IamCurlyHair@users.noreply.github.com>
2025-08-30 15:30:36 -0700

    Shadowflare full project

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -1,66 +1,151 @@
- using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class controller : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class HeroController : MonoBehaviour
 {
-    Animator animator;
-    // Start is called before the first frame update
+    [Header("Movement")]
+    public float walkSpeed = 16f;
+    public float sprintSpeed = 16f;
+    public float rotationSpeed = 12f;
+    public float jumpHeight = 1.4f;
+    public float gravity = -9.81f;
+
+    [Header("References")]
+    public Transform cameraTransform;
+    public Animator animator;
     public GameObject Effect1;
     public GameObject Effect2;
 
-    void Start()
+    [Header("Jump Effects")]
+    public GameObject jumpBurstVFX;  
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool sprintHeld;
+    private float yVelocity;
+    private bool isGrounded;
+
+    private PlayerControls controls;
+
+    private void Awake()
     {
-        animator = GetComponent<Animator>();
-        Debug.Log(animator);
+        controller = GetComponent<CharacterController>();
+        if (cameraTransform == null && Camera.main != null)
+            cameraTransform = Camera.main.transform;
+
+        
+        controls = new PlayerControls();
+
+        // Movement
+        controls.Gameplay.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        // Look 
+        controls.Gameplay.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Look.canceled += ctx => lookInput = Vector2.zero;
+
+        // Sprint
+        controls.Gameplay.Sprint.performed += ctx => sprintHeld = ctx.ReadValue<float>() > 0.5f;
+        controls.Gameplay.Sprint.canceled += ctx => sprintHeld = false;
+
+        // Jump
+        controls.Gameplay.Jump.performed += ctx => TryJump();
+
+        // Powers
+        controls.Gameplay.Power1.performed += ctx => DoPower1();
+      //  controls.Gameplay.Power2.performed += ctx => DoPower2();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() => controls.Enable();
+    private void OnDisable() => controls.Disable();
+
+    private void Update()
     {
-        bool isdancing = animator.GetBool("isdancing");
-        if (Input.GetKeyDown(KeyCode.Space)) // Check if space key is pressed down
+        
+        isGrounded = controller.isGrounded;
+        if (isGrounded && yVelocity < 0f) yVelocity = -2f;
+
+        Vector3 forward = Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up).normalized;
+        Vector3 right = Vector3.ProjectOnPlane(cameraTransform.right, Vector3.up).normalized;
+        Vector3 desired = forward * moveInput.y + right * moveInput.x;
+
+        float targetSpeed = sprintHeld ? sprintSpeed : walkSpeed;
+        Vector3 horizontal = desired.normalized * targetSpeed;
+
+        
+        if (desired.sqrMagnitude > 0.001f)
         {
-            if (!isdancing) // Start  dancing only if not already dancing
-            {
-                animator.SetBool("isdancing", true);
-                //Effect.SetActive(true);
-            }
+            Quaternion targetRot = Quaternion.LookRotation(desired, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
         }
 
-        if (isdancing && Input.GetKey("c"))
+        
+        yVelocity += gravity * Time.deltaTime;
+
+        
+        Vector3 velocity = new Vector3(horizontal.x, 0f, horizontal.z);
+        velocity.y = yVelocity;
+        controller.Move(velocity * Time.deltaTime);
+
+        // Animator params
+        if (animator)
         {
-            if (Effect2.activeSelf)
+            float speedParam = new Vector2(horizontal.x, horizontal.z).magnitude;
+            animator.SetFloat("Speed", speedParam);
+            animator.SetBool("IsGrounded", isGrounded);
+        }
+    }
+
+    private void TryJump()
+    {
+        if (!controller.isGrounded) return;
+
+        yVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        if (animator) animator.SetTrigger("Jump");
+
+        // Enable and play jump burst effect 
+        if (jumpBurstVFX != null)
+        {
+            jumpBurstVFX.SetActive(true); 
+            ParticleSystem ps = jumpBurstVFX.GetComponent<ParticleSystem>();
+            if (ps != null)
             {
-                Effect2.SetActive(false);
+                ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                ps.Play();
             }
-            Effect1.SetActive(true);
+
+            StartCoroutine(DisableJumpBurst()); 
         }
-        if (isdancing && Input.GetKey("b"))
-        {
-            Debug.Log(Effect1);
-            if (Effect1.activeSelf)
-            {
-                Debug.Log("effect 1 turned off");
-                Effect1.SetActive(false);
-            }
-            Effect2.SetActive(true);
-        }
-        else if (Input.anyKeyDown) // Check if any key is pressed down
-        {
-            if(isdancing) // Stop dancing only if currently dancing
-            {
-                animator.SetBool("isdancing", false);
-                if (Effect2.activeSelf){
-                    Effect2.SetActive(false);
-                }
-                if (Effect1.activeSelf){
-                    Effect1.SetActive(false);
-                }
-            }     
-        }
+    }
 
     
+    private IEnumerator DisableJumpBurst()
+    {
+        yield return new WaitForSeconds(1.5f); // match particle duration
+        if (jumpBurstVFX != null)
+            jumpBurstVFX.SetActive(false);
+    }
+
+
+
+
+
+    private void DoPower1()
+    {
+        if (animator) animator.SetTrigger("Power1");
+        if (Effect1) Effect1.SetActive(true);
+        if (Effect2) Effect2.SetActive(false);
+    }
     
-}
+    private void DoPower2()
+    {
+        if (animator) animator.SetTrigger("Power2");
+        if (Effect2) Effect2.SetActive(true);
+        if (Effect1) Effect1.SetActive(false);
+    }
+    
 }
